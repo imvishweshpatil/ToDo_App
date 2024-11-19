98a17c701d49eff1f5b31c58c659c8f9f3cffab9
Vishwesh Patil <vishveshpatil99@gmail.com>
2024-11-20 00:51:07 +0530

    Created ListTile.

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {
+          print('clicked on todo item');
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+        title: Text(
+          'Check mail',
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            onPressed: () {
+              print('clicked on delete icon');
+            },
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(
+              Icons.delete,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
